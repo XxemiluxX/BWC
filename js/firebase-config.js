b42d79ba98a8cb6b8e5a0dd1ed1d559016102b95
XxemiluxX <mongemoraemiliano60gmail.com>
2026-09-24 22:59:41 -0600

    inicio

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -14,5 +14,5 @@ export const firebaseConfig = {
   projectId: "bwc2026-f956e",
   storageBucket: "bwc2026-f956e.firebasestorage.app",
   messagingSenderId: "411769645050",
-  appId: "1:411769645050:web:PEGA_AQUI_TU_APP_ID"
+  appId: "1:411769645050:web:0e3f5c6b8d9e2f3c4b5a6d",
 };
